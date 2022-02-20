cf2c62de7e8e870a570caa5c6fff105f9a1b98c4
c53wei <c53wei@uwaterloo.ca>
2022-02-20 09:40:38 -0500

    Binary classifier i am confusion how to do multi pls help

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 % Part 2: Generating Clusters
 class_a = ClassData(200, [5 10]', [8 0; 0 4]);
-[class_a_x, class_a_y] = drawEllipse([5 10]', [8 0; 0 4]);
+[class_a_x, class_a_y] = class_a.drawEllipse([5 10]', [8 0; 0 4]);
 class_b = ClassData(200, [10, 15]', [8 0; 0 4]);
-[class_b_x, class_b_y] = drawEllipse([10, 15]', [8 0; 0 4]);
+[class_b_x, class_b_y] = class_b.drawEllipse([10, 15]', [8 0; 0 4]);
 
 figure;
 hold on;
@@ -12,6 +12,21 @@ scatter(class_b.Data(1, :), class_b.Data(2, :), 'r', 'filled');
 plot(class_a_x, class_a_y, 'b-', 'LineWidth', 2);	
 plot(class_b_x, class_b_y, 'r-', 'LineWidth', 2);	
 
+% Minimum Euclidean Distance
+% Use heuristics to guess how long line is
+data_range = max([class_a.Data class_b.Data], [], 2) ...
+    - min([class_a.Data class_b.Data], [], 2);
+l = norm(data_range)/2;
+
+
+mid = (class_a.Mean + class_b.Mean)/2;
+normal = class_b.Mean - class_a.Mean;
+normal = [-normal(2); normal(1)];
+normal = normal./norm(normal);
+p1 = mid + l*normal;
+p2 = mid - l*normal;
+line([p1(1), p2(1)], [p1(2), p2(2)], 'color','k','LineWidth',2)
+
 xlabel('x_1');
 ylabel('x_2');
 legend('Class A', 'Class B');
@@ -19,11 +34,11 @@ saveas(gcf, "img/class_a_b.png");
 hold off;
 
 class_c = ClassData(100, [5 10]', [8 4; 4 40]);
-[class_c_x, class_c_y] = drawEllipse([5 10]', [8 4; 4 40]);
+[class_c_x, class_c_y] = class_c.drawEllipse([5 10]', [8 4; 4 40]);
 class_d = ClassData(200, [15 10]', [8 0; 0 8]);
-[class_d_x, class_d_y] = drawEllipse([15 10]', [8 0; 0 8]);
+[class_d_x, class_d_y] = class_d.drawEllipse([15 10]', [8 0; 0 8]);
 class_e = ClassData(150, [10 5]', [10 -5; -5 20]);
-[class_e_x, class_e_y] = drawEllipse([10 5]', [10 -5; -5 20]);
+[class_e_x, class_e_y] = class_e.drawEllipse([10 5]', [10 -5; -5 20]);
 
 
 figure;
@@ -45,7 +60,6 @@ hold off;
 % Part 3: Classifiers
 
 
-% Minimum Euclidean Distance
 
-% mid = mean(
+
 
