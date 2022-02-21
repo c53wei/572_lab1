c56901ff7885b8abc1cfee4ee0583106c90013ff
c53wei <c53wei@uwaterloo.ca>
2022-02-21 17:34:10 -0500

    MAP classifier

--- main.m
+++ main.m
@@ -6,19 +6,25 @@ class_b = ClassData(200, [10, 15]', [8 0; 0 4]);
 
 figure;
 hold on;
+
+% Unit contour
+plot(class_a_x, class_a_y, 'b-', 'LineWidth', 2);	
+plot(class_b_x, class_b_y, 'r-', 'LineWidth', 2);
+
+% Minimum Euclidean Distance
+[x1, x2, classification] = ClassData.Classify([class_a class_b], 'MED');
+contour(x1, x2, classification, 'k');
+
+% Maximum a Posteriori 
+[x1, x2, classification] = ClassData.Classify([class_a class_b], 'MAP');
+contour(x1, x2, classification, 'k--');
+
 scatter(class_a.Data(1, :), class_a.Data(2, :), 'b', 'filled');
 scatter(class_b.Data(1, :), class_b.Data(2, :), 'r', 'filled');
 
-plot(class_a_x, class_a_y, 'b-', 'LineWidth', 2);	
-plot(class_b_x, class_b_y, 'r-', 'LineWidth', 2);	
-
-% Minimum Euclidean Distance
-[x1, x2, classification] = ClassData.MED([class_a class_b]);
-contour(x1, x2, classification, 'k', 'LineWidth', 2);
-
 xlabel('x_1');
 ylabel('x_2');
-legend('Class A', 'Class B');
+legend('Class A', 'Class B', 'MED', 'MAP');
 saveas(gcf, "img/class_a_b.png");
 hold off;
 
@@ -32,25 +38,30 @@ class_e = ClassData(150, [10 5]', [10 -5; -5 20]);
 
 figure;
 hold on; 
-scatter(class_c.Data(1, :), class_c.Data(2, :), 'b', 'filled');
-scatter(class_d.Data(1, :), class_d.Data(2, :), 'r', 'filled');
-scatter(class_e.Data(1, :), class_e.Data(2, :), 'k', 'filled');
+
 
 plot(class_c_x, class_c_y, 'b-', 'LineWidth', 2);
 plot(class_d_x, class_d_y, 'r-', 'LineWidth', 2);
 plot(class_e_x, class_e_y, 'k-', 'LineWidth', 2);
 
-[x1, x2, classification] = ClassData.MED([class_c class_d class_e]);
-contour(x1, x2, classification, 'k', 'LineWidth', 2);
+% Minimum Euclidean Distance
+[x1, x2, classification] = ClassData.Classify([class_c class_d class_e], 'MED');
+contour(x1, x2, classification, 'k');
 
+% Maximum a Posteriori
+[x1, x2, classification] = ClassData.Classify([class_c class_d class_e], 'MAP');
+contour(x1, x2, classification, 'k--');
+
+scatter(class_c.Data(1, :), class_c.Data(2, :), 'b', 'filled');
+scatter(class_d.Data(1, :), class_d.Data(2, :), 'r', 'filled');
+scatter(class_e.Data(1, :), class_e.Data(2, :), 'k', 'filled');
 
 xlabel('x_1');
 ylabel('x_2');
-legend('Class C', 'Class D', 'Class E');
+legend('Class C', 'Class D', 'Class E', 'MED', 'MAP');
 saveas(gcf, "img/class_c_d_e.png");
 hold off;
 
-% Part 3: Classifiers
 
 
 
