48b794d35cb57d1862331817aaa07d31226c59cb
KendrickLeemar <ken132132@gmail.com>
2022-02-26 16:05:41 -0500

    NN stuff initial commit

--- KL_main.m
+++ KL_main.m
@@ -28,6 +28,9 @@ y = min([class_a1(:,2);class_b1(:,2)])-1:step:max([class_a1(:,2);class_b1(:,2)])
 
 GED1 = GED(covar_a, mu_a, covar_b, mu_b, X1, Y1);
 
+NN1 = NN(1, class_a1, class_b1, X1, Y1);
+
+
 figure;
 hold on;
 
@@ -46,6 +49,9 @@ contour(x1, x2, classification, 'k--');
 %GED countour
 contour(X1,Y1,GED1, [0, 0], 'Color', 'green', 'LineWidth', 2);
 
+contourf(X1,Y1,NN1, [0, 0], 'Color', 'black', 'LineWidth', 2);
+contourf(X1,Y1,kNN1, [0, 0], 'Color', 'black', 'LineWidth', 2);
+
 scatter(class_a.Data(1, :), class_a.Data(2, :), 'b', 'filled');
 scatter(class_b.Data(1, :), class_b.Data(2, :), 'r', 'filled');
 
@@ -53,23 +59,23 @@ xlabel('x_1');
 ylabel('x_2');
 legend('Class A', 'Class B', 'MED', 'MAP', 'GED');
 saveas(gcf, "img/class_a_b.png");
-hold off;
-
-% Error Analysis
-expected1 = horzcat(repmat([1], 1, class_a.N), repmat([2], 1, class_b.N))';
-% Get MED classification results
-test_med1 = Classify([class_a class_b], horzcat(class_a.Data, class_b.Data), 'MED')';
-C = confusionmat(expected1, test_med1);
-error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
-confusionchart(C);
-title(sprintf('%s %s','MED–Error: ', num2str(error), '%'));
-saveas(gcf, "img/class_a_b_med_confusion.png");
-% Get MAP classification results
-test_map1 = Classify([class_a class_b], horzcat(class_a.Data, class_b.Data), 'MAP')';
-C = confusionmat(expected1, test_map1);
-error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
-confusionchart(C);
-title(sprintf('%s %s','MAP–Error: ', num2str(error), '%'));
+% hold off;
+% 
+% % Error Analysis
+% expected1 = horzcat(repmat([1], 1, class_a.N), repmat([2], 1, class_b.N))';
+% % Get MED classification results
+% test_med1 = Classify([class_a class_b], horzcat(class_a.Data, class_b.Data), 'MED')';
+% C = confusionmat(expected1, test_med1);
+% error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
+% confusionchart(C);
+% title(sprintf('%s %s','MED–Error: ', num2str(error), '%'));
+% saveas(gcf, "img/class_a_b_med_confusion.png");
+% % Get MAP classification results
+% test_map1 = Classify([class_a class_b], horzcat(class_a.Data, class_b.Data), 'MAP')';
+% C = confusionmat(expected1, test_map1);
+% error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
+% confusionchart(C);
+% title(sprintf('%s %s','MAP–Error: ', num2str(error), '%'));
 saveas(gcf, "img/class_a_b_map_confusion.png");
 
 %% Class C, Class D, & Class E
@@ -133,6 +139,25 @@ for i=1:size(X2, 1)
 end
 contour(X2, Y2, GED2, 'Color', 'green');
 
+NN_cd = NN(1, class_c1, class_d1, X2, Y2);
+NN_de = NN(1, class_d1, class_e1, X2, Y2);
+NN_ec = NN(1, class_e1, class_c1, X2, Y2);
+
+% All classes
+NN2 = zeros(size(X2, 1), size(Y2, 2));
+for i=1:size(X2, 1)
+    for j=1:size(Y2, 2)
+        if NN_cd(i, j) >= 0 && NN_de(i, j) <= 0
+            NN2(i, j) = 2;
+        elseif NN_cd(i, j) <= 0 && NN_ec(i, j) >= 0
+            NN2(i, j) = 1;
+        elseif NN_de(i, j) >= 0 && NN_ec(i, j) <= 0
+            NN2(i, j) = 3;
+        end
+    end
+end
+contourf(X2, Y2, NN2, 'Color', 'black');
+
 
 
 scatter(class_c.Data(1, :), class_c.Data(2, :), 'b', 'filled');
@@ -145,24 +170,24 @@ legend('Class C', 'Class D', 'Class E', 'MED', 'MAP', 'GED');
 saveas(gcf, "img/class_c_d_e.png");
 hold off;
 
-% Error Analysis
-expected2 = horzcat(repmat([1], 1, class_c.N), ...
-    repmat([2], 1, class_d.N), repmat([3], 1, class_e.N))';
-% Get MED classification results
-test_med2 = Classify([class_c class_d class_e], ...
-    horzcat(class_c.Data, class_d.Data, class_e.Data), 'MED')';
-C = confusionmat(expected2, test_med2);
-error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
-confusionchart(C);
-title(sprintf('%s %s','MED–Error: ', num2str(error), '%'));
-saveas(gcf, "img/class_c_d_e_med_confusion.png");
-% Get MAP classification results
-test_map2 = Classify([class_c class_d class_e], ...
-    horzcat(class_c.Data, class_d.Data, class_e.Data), 'MAP')';
-C = confusionmat(expected2, test_map2);
-error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
-confusionchart(C);
-title(sprintf('%s %s','MAP–Error: ', num2str(error), '%'));
+% % Error Analysis
+% expected2 = horzcat(repmat([1], 1, class_c.N), ...
+%     repmat([2], 1, class_d.N), repmat([3], 1, class_e.N))';
+% % Get MED classification results
+% test_med2 = Classify([class_c class_d class_e], ...
+%     horzcat(class_c.Data, class_d.Data, class_e.Data), 'MED')';
+% C = confusionmat(expected2, test_med2);
+% error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
+% confusionchart(C);
+% title(sprintf('%s %s','MED–Error: ', num2str(error), '%'));
+% saveas(gcf, "img/class_c_d_e_med_confusion.png");
+% % Get MAP classification results
+% test_map2 = Classify([class_c class_d class_e], ...
+%     horzcat(class_c.Data, class_d.Data, class_e.Data), 'MAP')';
+% C = confusionmat(expected2, test_map2);
+% error = (sum(C, 'all') - trace(C))/sum(C, 'all') * 100;
+% confusionchart(C);
+% title(sprintf('%s %s','MAP–Error: ', num2str(error), '%'));
 saveas(gcf, "img/class_c_d_e_map_confusion.png");
 
 
---
+++ NN.m
@@ -0,0 +1,26 @@
+function[ dist ] = NN( k, class_a, class_b, X, Y )
+    dist = zeros(size(X, 1), size(Y, 2));
+    for i = 1:size(X, 1)
+       for j = 1:size(Y, 2)
+           point = [X(i,j), Y(i,j)];
+           dist(i, j) = get_closest(point, class_a, k) - get_closest(point, class_b, k);
+       end
+    end
+end
+
+function [closest] = get_closest(point, samples, k)
+    dist = zeros(size(samples,1),1);
+    for i = 1:size(samples, 1)
+        x = samples(i,:);
+        dist(i,1) = sqrt( ( x(1,1) - point(1,1) )^2 + ( x(1,2) - point(1,2) )^2 ); 
+    end    
+    distanceMatrix = [dist, samples];
+    distanceMatrix = sortrows(distanceMatrix);
+    
+    %if k ==1 , NN else its kNN
+    distanceMatrix = distanceMatrix(1:k,:);
+    distanceMatrix(:,1) = []; 
+    closest_mean = mean(distanceMatrix, 1);
+    closest = sqrt((point - closest_mean) * (point-closest_mean)'); %take the distance to the closest mean.
+end
+
