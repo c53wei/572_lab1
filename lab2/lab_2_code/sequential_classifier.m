37603a83729072ea4458f863e7944e4646bc98f2
Victoria Li <v42li@uwaterloo.ca>
2022-03-29 23:12:46 -0400

    section 4 updates

--- lab2/lab_2_code/sequential_classifier.m
+++ lab2/lab_2_code/sequential_classifier.m
@@ -2,10 +2,13 @@ function [discriminant, error] = sequential_classifier(a,b,X,Y)
     orig_a = a;
     orig_b = b;
     
+    na = 0;
+    nb = 0;
+    
     j = 1;
     prototypes_A = double.empty(0,2);
     prototypes_B = double.empty(0,2);
-
+ 
     while true
         prototype_A = a(randi(size(a,1)),:);
         prototype_B = b(randi(size(b,1)),:);
@@ -18,22 +21,57 @@ function [discriminant, error] = sequential_classifier(a,b,X,Y)
         
         prototypes_A = vertcat(prototypes_A, prototype_A);
         prototypes_B = vertcat(prototypes_B, prototype_B);
-
-        if conf_matrix(1,2) == 0
-            % remove the true Bs from b
+        naB = conf_matrix(1,2);
+        nbA = conf_matrix(2,1);
+ 
+        if naB == 0
+            prototypes_B = setxor(prototypes_B, classified_B, 'rows');
         end
             
-        if conf_matrix(2,1) == 0
-            % remove true As from a
+        if nbA == 0
+            prototypes_A = setxor(prototypes_A, classified_A, 'rows');
         end
-
+ 
         if(size(a, 1) == 0 || size(b,1) == 0)
             % stop
             break;
         end
     end
-
+ 
     discriminant = zeroes(size(X));
-
+    for count = 1:size(orig_a,1)
+        x_test = orig_a(k,:);
+        for i = 1:j
+            correct_a = MED_true_or_false(prototypes_A(i,:), prototypes_B(i,:), x_test);
+            if correct_a
+                na = na + 1;
+                break
+            end
+        end
+    end
     
+    for count = 1:size(orig_b,1)
+        x_test = orig_b(k,:);
+        for i = i:j
+            correct_a = MED_true_or_false(prototypes_A(i,:), prototypes_B(i,:), x_test);
+            if ~correct_a
+                nb = nb + 1;
+                break
+            end
+        end
+    end
+    
+    error = 1 - (na+nb)/400;
+    
+    end
+ 
+function [class_A] = MED_true_or_false(prototypeA, prototypeB, x)
+    dist_A = sqrt((prototypeA(1)-x(1)).^2 + (prototypeA(2)-x(2)).^2);
+    dist_B = sqrt((prototypeB(1)-x(1)).^2 + (prototypeB(2)-x(2)).^2);
+    
+    if dist_A < dist_B
+        class_A = true;
+    else
+        class_A = false;
+    end
 end
